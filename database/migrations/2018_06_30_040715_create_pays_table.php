95fcb11c440e02632374e2035ec247867d91240c
Nantapong_k <nuntapong@owndays.com>
2018-06-30 11:27:06 +0700

    initial migration and auth

--- database/migrations/2018_06_30_040715_create_pays_table.php
+++ database/migrations/2018_06_30_040715_create_pays_table.php
@@ -15,7 +15,12 @@ class CreatePaysTable extends Migration
     {
         Schema::create('pays', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('kind_id')->unsigned();
+            $table->string('description');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('kind_id')->references('id')->on('kinds')->onDelete('restrict');
         });
     }
 
