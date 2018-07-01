125096e8c9b33df5a084877fe40a20c5e74349d1
Nantapong_k <nuntapong@owndays.com>
2018-07-01 10:19:34 +0700

    vue pagination

--- app/Http/Controllers/api/v1/PayController.php
+++ app/Http/Controllers/api/v1/PayController.php
@@ -2,19 +2,16 @@
 
 namespace App\Http\Controllers\api\v1;
 
+use App\Http\Resources\PayCollection;
 use Illuminate\Routing\Controller as BaseController;
-use App\Pay;
 use App\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 
 class PayController extends BaseController
 {
     public function index()
     {
         $user = User::find(1);
-        $pays = $user->pays()->with("kind")->get();
-        return response()->json($pays);
+        $pays = $user->pays()->with("kind")->paginate(10);
+        return new PayCollection($pays);
     }
 }
--- resources/assets/js/components/PayTableComponent.vue
+++ resources/assets/js/components/PayTableComponent.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
+        <div class="pagination">
+            <button class="btn btn-default" @click="getData(pagination.prev_page_url)"
+                    :disabled="!pagination.prev_page_url">
+                Previous
+            </button>
+            <span>Page {{pagination.current_page}} of {{pagination.last_page}}</span>
+            <button class="btn btn-default" @click="getData(pagination.next_page_url)"
+                    :disabled="!pagination.next_page_url">Next
+            </button>
+        </div>
         <table class="table">
             <thead>
                 <th>Date</th>
@@ -23,19 +33,31 @@
     export default {
         data: function() {
             return {
-                pays: []
+                pays: [],
+                pagination: {}
             }
         },
         mounted: function() {
-           this.getData();     
+            let url = route('api.pays.index');
+            this.getData(url);n
         },
         methods: {
-            getData: function() {
-                let url = route('api.pays.index');
+            getData: function(url) {
+
                 let vm = this;
                 axios.get(url).then( function(res) {
-                    vm.pays = res.data;            
+                    vm.pays = res.data.data;
+                    vm.makePagination(res.data);
                 });
+            },
+            makePagination: function(data) {
+                let pagination = {
+                    current_page: data.meta.current_page,
+                    last_page: data.meta.last_page,
+                    next_page_url: data.links.next,
+                    prev_page_url: data.links.prev
+                }
+                this.pagination = pagination;
             }
         }
     }
